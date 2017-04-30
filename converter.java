12a5dae906617c184c4a9bf7494ff06d5ee10d3d
Rebecca Ramnauth <rramnauth2220@bths.edu>
2017-04-30 11:56:09 -0400

    Add files via upload

---
+++ converter.java
@@ -0,0 +1,284 @@
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.io.FileNotFoundException;
+
+
+/**
+ * Convert New York City Administrative Code XML files to Excel Spreadsheet
+ * @imports Apache POI, Apache Commons, W3C libraries
+ * @author Rebecca Ramnauth
+ * Date: 4-22-2017
+ */
+public class converter {
+    private static Workbook workbook;
+    private static int rowNum;
+	
+	private final static int FILE_NAME = 0;
+	
+	//LEVEL 1 : TITLE	
+	private final static int RECORD_ID = 1; 		//TITLE attribute id
+	private final static int RECORD_NUMBER = 2;		//TITLE attribute number
+	private final static int RECORD_VERSION = 3;	//TITLE attribute version
+	private final static int TITLE_NUM = 4; 		//HEADING, Regex excluding alpha characters
+	private final static int TITLE_SUBJECT = 5;		//HEADING, substring of everything beyond the ': '
+	
+	//LEVEL 2 : CHAPTER
+	private final static int C_RECORD_ID = 6;		//RECORD attribute id
+	private final static int C_RECORD_NUMBER = 7;	//RECORD attribute number
+	private final static int C_RECORD_VERSION = 8;	//RECORD attribute version
+	private final static int CHAPTER_NUM = 9;		//HEADING, Regex excluding alpha characters
+	private final static int CHAPTER_SUBJECT = 10;	//HEADING, substring of everything beyond the ': '
+	
+	//LEVEL 3 : SUBCHAPTER
+	private final static int HAS_SUBCHAPTER = 11;
+	
+	//LEVEL 4 : APPENDIX
+	private final static int HAS_APPENDIX = 12;
+	
+	//LEVEL 5 : PART
+	private final static int HAS_PART = 13;
+	
+	//LEVEL 6 : ARTICLE
+	private final static int HAS_ARTICLE = 14;
+	
+	//LEVEL 7 : SUBARTICLE
+	private final static int HAS_SUBARTICLE = 15;
+	
+	//LEVEL 8 : SECTION
+	private final static int S_RECORD_ID = 16;		//RECORD attribute id
+	private final static int S_RECORD_NUMBER = 17;	//RECORD attribute number
+	private final static int S_RECORD_VERSION = 18;	//RECORD attribute version
+	private final static int SECTION_NUM = 19;		//HEADING, Regex excluding alpha characters
+	private final static int SECTION_SUBJECT = 20;	//HEADING, substring of everything beyond the ': '
+	
+	//LEVEL 9 : PARAGRAPH
+	private final static int PARAGRAPH = 21;		//PARA
+	
+	//NEXT STEPS
+	private final static int CITATION = 22;
+
+    public static void main(String[] args) throws Exception {
+        //retrieveAndReadXml(); //test: read the first file and extract data.
+        read();
+    }
+    
+    /**
+     * Defines possibilities of the file name given the first
+     * Calls retrieveAndReadXml() to actually undergo the conversion process
+     * @param
+     
+	private static void possibility() throws Exception{
+		File home = new File ("C:\\Users\\Shahendra\\Documents\\JCreator Pro\\MyProjects\\excel-xml-nycac\\admin_Test");
+		File[] files = home.listFiles();
+		int total = 0;
+		for (int i = 0; i < files.length; i++){
+			read(files[i]);
+			total++;
+		}
+		System.out.println("TOTAL " + total);
+	}
+    */
+	
+    private static void read() throws Exception{
+    	Cell cell;
+    	    	
+    	initXls();
+    	Sheet sheet = workbook.getSheetAt(0);
+    	
+    	File origin = new File ("C:\\Users\\Shahendra\\Documents\\JCreator Pro\\MyProjects\\excel-xml-nycac\\admin_Test");
+    	File[] files = origin.listFiles();
+    	int count = 0;
+    	
+    	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+    	
+    	for (File file : files){	
+    		System.out.println("Reading " + file.getAbsolutePath());
+    		Document doc = dBuilder.parse(file);
+    		
+	    	NodeList title = doc.getElementsByTagName("LEVEL");
+	    	for (int a = 0; a < title.getLength(); a++){
+	    		Node home = title.item(a);
+	    		if (home.getNodeType() == Node.ELEMENT_NODE){
+	    			Element name = (Element) home;
+	    			String chapter;
+	    			try{
+	    				chapter = name.getElementsByTagName("HEADING").item(0).getTextContent(); //pulls all headings (i.e., title, chapter, sub., section) causing duplicates in paragraphing
+	    			}
+	    			catch(NullPointerException e){
+	    				chapter = "NULL";
+	    			}
+	    			
+	    			NodeList para = name.getElementsByTagName("PARA");			
+	    			for (int b = 0; b < para.getLength(); b++){
+	    				Node parag = para.item(b);
+	    				if (parag.getNodeType() == Node.ELEMENT_NODE){
+	    					Element paragrapf = (Element) parag;
+	    					String requirement = paragrapf.getTextContent();
+	    				
+	    					Row row = sheet.createRow(rowNum++);;
+	    					
+	    					cell = row.createCell(CHAPTER_NUM);
+			    			cell.setCellValue(chapter);
+			    			
+			    			cell = row.createCell(FILE_NAME);
+			    			cell.setCellValue(file.getAbsolutePath());
+			    			
+			    			cell = row.createCell(PARAGRAPH);
+			    			cell.setCellValue(requirement);
+	    				}
+	    			}
+	    		}
+	    	}
+	    	count++;
+    	}
+       	System.out.println("TOTAL: " + count);
+       	
+        FileOutputStream fileOut = new FileOutputStream("example.xlsx");
+        workbook.write(fileOut);
+        workbook.close();
+        fileOut.close();
+    	
+    }
+
+    /**
+     * Initializes the POI workbook and writes the header row
+     */
+    private static void initXls() {
+        workbook = new XSSFWorkbook();
+
+        CellStyle style = workbook.createCellStyle();
+        Font boldFont = workbook.createFont();
+        boldFont.setBold(true);
+        style.setFont(boldFont);
+        style.setAlignment(CellStyle.ALIGN_CENTER);
+
+        Sheet sheet = workbook.createSheet();
+        rowNum = 0;
+        Row row = sheet.createRow(rowNum++);
+        
+        //private final static int FILE_NAME = 0;
+        Cell cell = row.createCell(FILE_NAME);
+        cell.setCellValue("File");
+        cell.setCellStyle(style);
+	
+		//LEVEL 1 : TITLE	
+		//private final static int RECORD_ID = 0; 		//TITLE attribute id
+		cell = row.createCell(RECORD_ID);
+        cell.setCellValue("Title ID");
+        cell.setCellStyle(style);
+		//private final static int RECORD_NUMBER = 0;	//TITLE attribute number
+		cell = row.createCell(RECORD_NUMBER);
+        cell.setCellValue("Title Record");
+        cell.setCellStyle(style);
+		//private final static int RECORD_VERSION = 0;	//TITLE attribute version
+		cell = row.createCell(RECORD_VERSION);
+        cell.setCellValue("Title Version");
+        cell.setCellStyle(style);
+		//private final static int TITLE_NUM = 1; 		//HEADING, Regex excluding alpha characters
+		cell = row.createCell(TITLE_NUM);
+        cell.setCellValue("Title #");
+        cell.setCellStyle(style);
+		//private final static int TITLE_SUBJECT = 1;		//HEADING, substring of everything beyond the ': '
+		cell = row.createCell(TITLE_SUBJECT);
+        cell.setCellValue("Title Subject");
+        cell.setCellStyle(style);
+		
+		//LEVEL 2 : CHAPTER
+		//private final static int C_RECORD_ID = 0;		//RECORD attribute id
+		cell = row.createCell(C_RECORD_ID);
+        cell.setCellValue("Chapter ID");
+        cell.setCellStyle(style);
+		//private final static int C_RECORD_NUMBER = 0;	//RECORD attribute number
+		cell = row.createCell(C_RECORD_NUMBER);
+        cell.setCellValue("Chapter Record");
+        cell.setCellStyle(style);
+		//private final static int C_RECORD_VERSION = 0;	//RECORD attribute version
+		cell = row.createCell(C_RECORD_VERSION);
+        cell.setCellValue("Chapter Version");
+        cell.setCellStyle(style);
+		//private final static int CHAPTER_NUM = 0;		//HEADING, Regex excluding alpha characters
+		cell = row.createCell(CHAPTER_NUM);
+        cell.setCellValue("Chapter #");
+        cell.setCellStyle(style);
+		//private final static int CHAPTER_SUBJECT = 0;	//HEADING, substring of everything beyond the ': '
+		cell = row.createCell(CHAPTER_SUBJECT);
+        cell.setCellValue("Chapter Subject");
+        cell.setCellStyle(style);
+		
+		//LEVEL 3 : SUBCHAPTER
+		//private final static int HAS_SUBCHAPTER = 0;
+		cell = row.createCell(HAS_SUBCHAPTER);
+        cell.setCellValue("Subchapter Exists?");
+        cell.setCellStyle(style);
+		
+		//LEVEL 4 : APPENDIX
+		//private final static int HAS_APPENDIX = 0;
+		cell = row.createCell(HAS_APPENDIX);
+        cell.setCellValue("Appendix Exists?");
+        cell.setCellStyle(style);
+		
+		//LEVEL 5 : PART
+		//private final static int HAS_PART = 0;
+		cell = row.createCell(HAS_PART);
+        cell.setCellValue("Part Exists?");
+        cell.setCellStyle(style);
+		
+		//LEVEL 6 : ARTICLE
+		//private final static int HAS_ARTICLE = 0;
+		cell = row.createCell(HAS_ARTICLE);
+        cell.setCellValue("Article Exists?");
+        cell.setCellStyle(style);
+		
+		//LEVEL 7 : SUBARTICLE
+		//private final static int HAS_SUBARTICLE = 0;
+		cell = row.createCell(HAS_SUBARTICLE);
+        cell.setCellValue("Subarticle Exists?");
+        cell.setCellStyle(style);
+		
+		//LEVEL 8 : SECTION
+		//private final static int S_RECORD_ID = 0;		//RECORD attribute id
+		cell = row.createCell(S_RECORD_ID);
+        cell.setCellValue("Section ID");
+        cell.setCellStyle(style);
+		//private final static int S_RECORD_NUMBER = 0;	//RECORD attribute number
+		cell = row.createCell(S_RECORD_NUMBER);
+        cell.setCellValue("Section Record");
+        cell.setCellStyle(style);
+		//private final static int S_RECORD_VERSION = 0; 	//RECORD attribute version
+		cell = row.createCell(S_RECORD_VERSION);
+        cell.setCellValue("Section Version");
+        cell.setCellStyle(style);
+		//private final static int SECTION_NUM = 0;		//HEADING, Regex excluding alpha characters
+		cell = row.createCell(SECTION_NUM);
+        cell.setCellValue("Section #");
+        cell.setCellStyle(style);
+		//private final static int SECTION_SUBJECT = 0;	//HEADING, substring of everything beyond the ': '
+		cell = row.createCell(SECTION_SUBJECT);
+        cell.setCellValue("Section Subject");
+        cell.setCellStyle(style);
+		
+		//LEVEL 9 : PARAGRAPH
+		//private final static int PARAGRAPH = 0;			//PARA
+		cell = row.createCell(PARAGRAPH);
+        cell.setCellValue("Subrequirement");
+        cell.setCellStyle(style);
+		
+		//NEXT STEPS
+		//private final static int CITATION = 0;
+	    cell = row.createCell(CITATION);
+        cell.setCellValue("Citation");
+        cell.setCellStyle(style);
+    }
+}
---
+++ example_converter.java
@@ -0,0 +1,269 @@
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+
+/**
+ * Convert XML file to Excel Spreadsheet
+ * @imports Apache POI, Apache Commons, W3C libraries
+ * @author Rebecca Ramnauth
+ * Date: 3-3-2017
+ */
+public class example_converter {
+    private static Workbook workbook;
+    private static int rowNum;
+
+    private final static int TITLE_NUM_COLUMN = 0;
+    private final static int TITLE_SUBJECT = 1;
+    private final static int TITLE_PARTS = 2;
+    private final static int TITLE_REVISION_DATE = 3;
+    private final static int TITLE_CONTAINS = 4;
+    private final static int TITLE_DATE_PUBLISHED = 5;  
+    private final static int SUBTITLE_NAME_COLUMN = 6;
+    //private final static int CHAPTER_NUM_COLUMN = 7;
+    //private final static int PART_NUM_COLUMN = 8;
+    //private final static int PART_NAME_COLUMN = 9;
+    private final static int SUBPART_COLUMN = 7;
+    private final static int SECTION_NUM_COLUMN = 8;
+    private final static int SECTION_NAME_COLUMN = 9;
+    private final static int SUBREQ_PARA_COLUMN = 10;
+    private final static int CITA_COLUMN = 11;
+
+    public static void main(String[] args) throws Exception {
+        retrieveAndReadXml();
+    }
+
+private static void retrieveAndReadXml() throws Exception {
+    	Cell cell; 
+        System.out.println("Completed read of XML file");
+
+        /* File xmlFile = File.createTempFile("substances", "tmp");
+         * String xmlFileUrl = "http://whatever";
+         * URL url = new URL(xmlFileUrl);
+         * System.out.println("Downloading file from " + xmlFileUrl + " ...");
+         * FileUtils.copyURLToFile(url, xmlFile);
+         * System.out.println("Downloading COMPLETED. Parsing...");
+
+         If the XML file is stored externally, uncomment the above code. 
+         */
+        File xmlFile = new File("CFR-2016-title49-vol9.xml");
+
+        //initXls();
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(xmlFile);
+		
+		NodeList tList = doc.getElementsByTagName("TITLEPG");
+		for (int a = 0; a < tList.getLength(); a++){
+			System.out.println("Processing TITLE " + (a + 1) + "/" + tList.getLength());
+			Node home = tList.item(a);
+			if (home.getNodeType() == Node.ELEMENT_NODE){
+				Element title = (Element) home;
+				String title_num, subject, included_parts, revision_date, contain_descrip, date_published;
+				
+				try {
+					title_num = title.getElementsByTagName("TITLENUM").item(0).getTextContent();
+				} catch (NullPointerException e){
+					title_num = "";
+				}
+				try {
+					subject = title.getElementsByTagName("SUBJECT").item(0).getTextContent();
+				} catch (NullPointerException e){
+					subject = "";
+				}
+				try {
+					included_parts = title.getElementsByTagName("PARTS").item(0).getTextContent();
+				} catch (NullPointerException e){
+					included_parts = "";
+				}
+				try {
+					revision_date = title.getElementsByTagName("REVISED").item(0).getTextContent();
+				} catch (NullPointerException e){
+					revision_date = "";
+				}
+				try {
+					contain_descrip = title.getElementsByTagName("CONTAINS").item(0).getTextContent();
+				} catch (NullPointerException e){
+					contain_descrip = "";
+				}
+				try {
+					date_published = title.getElementsByTagName("DATE").item(0).getTextContent();
+				} catch (NullPointerException e){
+					date_published = "";
+				}
+								
+				NodeList subList = doc.getElementsByTagName("SUBTITLE");
+				if (subList.getLength() == 0){
+					//alternate = true;
+				}
+				for (int b = 0; (b < subList.getLength()); b++){
+					System.out.println("Processing SUBTITLE " + (b + 1) + "/" + subList.getLength());
+					Node subhome = subList.item(b);
+					if (subhome.getNodeType() == Node.ELEMENT_NODE){
+						Element subtitle = (Element) subhome;
+						String hd_source;
+						
+						try{
+							hd_source = subtitle.getElementsByTagName("HD").item(0).getTextContent();
+						} catch (NullPointerException e){
+							hd_source = "";
+						}
+						
+						//NodeList cList = subtitle.getElementsByTagName("CHAPTER");
+						//for (int c = 0; c < cList.getLength(); c++){
+						//System.out.println("Processing CHAPTER " + (c + 1) + "/" + cList.getLength());
+						//Node chap = cList.item(c);
+						//if (chap.getNodeType() == Node.ELEMENT_NODE){
+							//Element chapter = (Element) chap;
+						
+					        NodeList nList = doc.getElementsByTagName("SUBPART");
+					        if (nList.getLength() == 0){
+								//alternate = true;
+							}
+					        for (int i = 0; i < nList.getLength(); i++) {
+					            System.out.println("Processing SUBPART " + (i + 1) + "/" + nList.getLength());
+					            Node node = nList.item(i);
+					            if (node.getNodeType() == Node.ELEMENT_NODE) {
+					                Element element = (Element) node;
+					                String header, section_num, sb_subject;
+					                
+					                try {
+					                	header = element.getElementsByTagName("HD").item(0).getTextContent();
+					                } catch (NullPointerException e){
+					                	header = " ";
+					                }
+					
+					                NodeList prods = element.getElementsByTagName("SECTION");
+					                for (int j = 0; j < prods.getLength(); j++) {
+					                	System.out.println("	Processing SECTION " + (j + 1) + "/" + prods.getLength());
+					                    Node prod = prods.item(j);
+					                    if (prod.getNodeType() == Node.ELEMENT_NODE) {
+					                        Element product = (Element) prod;
+					                        String ss_num, ss_subject, citation;
+					                        
+					                        try{
+					                        	ss_num = product.getElementsByTagName("SECTNO").item(0).getTextContent();
+					                        } catch (NullPointerException e){
+					                        	ss_num = "";
+					                        }
+					                        try{
+					                        	ss_subject = product.getElementsByTagName("SUBJECT").item(0).getTextContent();
+					                        } catch (NullPointerException e){
+					                        	ss_subject = "";
+					                        }
+					                        try{
+					                        	citation = product.getElementsByTagName("CITA").item(0).getTextContent();
+					                        } catch (NullPointerException e){
+					                        	citation = "";
+					                        }
+					                        
+					                        NodeList sub_requirements = product.getElementsByTagName("P");
+					                        for (int k = 0; k < sub_requirements.getLength(); k++){
+					                        	System.out.println("		Processing SUB-REQUIREMENT " + (k + 1) + "/" + sub_requirements.getLength());
+					                        	Node req = sub_requirements.item(k);
+					                        	if (req.getNodeType() == Node.ELEMENT_NODE){
+					                        		Element requirement = (Element) req;
+					                        		String ss_para = requirement.getTextContent();
+					                        		
+					                        		Row row = sheet.createRow(rowNum++);
+					                    
+							                        //TITLE DETAILS
+							                        cell = row.createCell(TITLE_NUM_COLUMN);
+							                        if (title_num != null && !title_num.equals(""))
+							                        	cell.setCellValue(title_num);
+							                        else
+							                        	cell.setCellValue(" ");
+							                        
+							                        cell = row.createCell(TITLE_SUBJECT);
+							                        if (subject != null && !subject.equals(""))
+							                        	cell.setCellValue(subject);
+							                        else
+							                        	cell.setCellValue(" ");
+							                        
+							                        cell = row.createCell(TITLE_PARTS);
+							                        if (included_parts != null && !included_parts.equals(""))
+							                        	cell.setCellValue(included_parts);
+							                        else
+							                        	cell.setCellValue(" ");
+							                        
+							                        cell = row.createCell(TITLE_REVISION_DATE);
+							                        if (revision_date != null && !revision_date.equals(""))
+							                        	cell.setCellValue(revision_date);
+							                        else
+							                        	cell.setCellValue(" ");
+							                        
+							                        cell = row.createCell(TITLE_CONTAINS);
+							                        if (contain_descrip != null && !contain_descrip.equals(""))
+							                        	cell.setCellValue(contain_descrip);
+							                        else
+							                        	cell.setCellValue(" ");
+							                        
+							                        cell = row.createCell(TITLE_DATE_PUBLISHED);
+							                        if (date_published != null && !date_published.equals(""))
+							                        	cell.setCellValue(date_published);
+							                        else
+							                        	cell.setCellValue(" ");
+							                        
+							                        //SUBTITLE DETAILS
+							                        cell = row.createCell(SUBTITLE_NAME_COLUMN);
+							                        if (hd_source != null && !hd_source.equals(""))
+							                        	cell.setCellValue(hd_source);
+							                        else
+							                        	cell.setCellValue(" ");
+							                        
+							                        //SUBPART DETAILS
+							                        cell = row.createCell(SUBPART_COLUMN);
+							                        if (header != null && !header.equals(""))
+							                        	cell.setCellValue(header);
+							                        else
+							                        	cell.setCellValue(" ");
+							                        	
+													//SECTION DETAILS
+							                        cell = row.createCell(SECTION_NUM_COLUMN);
+							                        if (ss_num != null && !ss_num.equals(""))
+							                        	cell.setCellValue(ss_num);
+							                        else
+							                        	cell.setCellValue(" ");
+							                        	
+							                        cell = row.createCell(SECTION_NAME_COLUMN);
+							                        if (ss_subject != null && !ss_subject.equals(""))
+							                        	cell.setCellValue(ss_subject);
+							                        else
+							                        	cell.setCellValue(" ");
+							
+							                        cell = row.createCell(SUBREQ_PARA_COLUMN);
+							                        if (ss_para != null && !ss_para.equals(""))
+							                        	cell.setCellValue(ss_para);
+							                        else
+							                        	cell.setCellValue(" ");
+							
+							                        cell = row.createCell(CITA_COLUMN);
+							                        if (citation != null && !citation.isEmpty())
+							                        	cell.setCellValue(citation);
+							                        else
+							                        	cell.setCellValue(" ");
+					                        	}
+					                        }
+					                    }
+					                }
+					            }
+					        }
+				    	}
+					}
+				}
+			}
+	}
+}
